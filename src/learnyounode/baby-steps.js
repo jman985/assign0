36c49905978f7b6165baa88331ece07515c26109
jman985 <jman985@gmail.com>
2022-03-27 14:27:50 -0500

    added  node exercise 3

--- src/learnyounode/baby-steps.js
+++ src/learnyounode/baby-steps.js
@@ -1,10 +1,8 @@
-//console.log(process.argv) //
-
-let sum = 0
+let sum = 0;
 
 for (let i=2;i<process.argv.length;i++){
 
     sum=sum + Number(process.argv[i])
-}
+};
 
-console.log(sum)
+console.log(sum);
--- src/learnyounode/my-first-io.js
+++ src/learnyounode/my-first-io.js
@@ -1,1 +1,20 @@
-// TODO: Replace this with your solution.
+const fs = require('fs');
+
+const buf = fs.readFileSync(process.argv[2]);
+
+const str = buf.toString();
+
+const lineSplit = str.split("\n")
+let lineCount = 0
+
+for(i=0; i<lineSplit.length; i++){
+
+    if(lineSplit[i]=='\r'){
+        lineCount++
+    }
+}
+//console.log(str)
+
+//console.log(lineSplit)
+
+console.log(lineCount)
