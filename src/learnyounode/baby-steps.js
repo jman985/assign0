1e7d13d4bfe73fac841e44f43e776861d10f166c
jman985 <jman985@gmail.com>
2022-03-27 13:48:26 -0500

    completed 2 node excercises

--- src/learnyounode/baby-steps.js
+++ src/learnyounode/baby-steps.js
@@ -1,1 +1,10 @@
-// TODO: Replace this with your solution.
+//console.log(process.argv) //
+
+let sum = 0
+
+for (let i=2;i<process.argv.length;i++){
+
+    sum=sum + Number(process.argv[i])
+}
+
+console.log(sum)
