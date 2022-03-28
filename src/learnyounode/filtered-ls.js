90b56d88606d53ffc4e74f84ce4af28048ae73df
jman985 <jman985@gmail.com>
2022-03-27 21:06:06 -0500

    added exercise 5

--- src/learnyounode/filtered-ls.js
+++ src/learnyounode/filtered-ls.js
@@ -1,1 +1,17 @@
-// TODO: Replace this with your solution.
+const fs = require('fs')
+const path = require('path');
+
+const directory = process.argv[2];
+const ext = '.' + process.argv[3];
+
+fs.readdir(directory, function (err, files) {
+    if (err) {
+        return console.log(err);
+    }
+
+    files.forEach(function(file) {
+        if (path.extname(file) === ext) {
+            console.log(file);
+        };
+    });
+});
--- src/learnyounode/my-first-async-io.js
+++ src/learnyounode/my-first-async-io.js
@@ -1,1 +1,13 @@
-// TODO: Replace this with your solution.
+var fs = require('fs');
+
+fs.readFile(process.argv[2], function(err, data) {
+    if (err) {
+        return console.log(err)
+    }
+    
+    {
+        const str = data.toString();
+        const strArray = str.split('\n');
+        console.log(strArray.length - 1);
+    };
+});
