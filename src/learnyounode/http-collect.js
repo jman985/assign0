2adff07b6b1c069df5d4eaa71fd3ab7472d62dc8
jman985 <jman985@gmail.com>
2022-03-27 21:47:51 -0500

    added http-collect

--- src/learnyounode/http-collect.js
+++ src/learnyounode/http-collect.js
@@ -1,1 +1,15 @@
-// TODO: Replace this with your solution.
+const http = require('http');
+const bl = require('bl');
+
+const url = process.argv[2];
+
+http.get(url, function (response) {
+    // Use 'bl' package to collect entire stream of data
+    response.pipe(bl(function(err, data) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log(data.toString().length);
+        console.log(data.toString());
+    }));
+});
